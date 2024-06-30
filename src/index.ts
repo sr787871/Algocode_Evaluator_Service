365148f9b96c8dac6a19d501bb0029974442c1e0
Sachin Rawat <sr787871@gmail.com>
2024-06-30 16:17:32 +0530

    Added Eslint Configuration + Added Basic api setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
-import serverConfig from './config/serverConfig';
-serverConfig
-const app = express();
+import express,{ Express } from "express";
+
+import serverConfig from "./config/serverConfig";
+import apiRouter from "./routes";
+
+const app : Express = express();
+
+app.use('/api',apiRouter);
 
 app.listen(serverConfig.PORT,()=>{
-    console.log(`Server started at ${serverConfig.PORT}`)
-})
+    console.log(`Server started at ${serverConfig.PORT}`);
+});
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import { PingController } from '../../controllers/pingController';
+
+const v1Router = express.Router();
+
+v1Router.get('/ping', PingController);
+
+export default v1Router;
