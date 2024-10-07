decc278dd061f3c27393a2b9aac751ecb1a7fcd0
Sachin Rawat <sr787871@gmail.com>
2024-10-07 13:56:35 +0530

    Bull Board UI setup complete

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express,{ Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -8,9 +9,11 @@ import SampleWorker from "./workers/sampleWorker";
 const app : Express = express();
 
 app.use('/api',apiRouter);
+app.use('/ui',bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT,()=>{
     console.log(`Server started at ${serverConfig.PORT}`);
+    console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
 
     SampleWorker("SampleQueue");
 
